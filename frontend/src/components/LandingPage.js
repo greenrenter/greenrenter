0c7abbdf7ad272dabbaf3ed6b3cf775740c128c9
Steven Vi <visteven00@gmail.com>
2019-06-22 13:48:59 +1000

    added styled components npm package

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    h1 {
+        color: red;
+    }
+`;
 
 class LandingPage extends Component {
     render() {
         return (
-            <div>
+            <Wrapper>
                 <h1>Landing Page</h1>
                 <Link to="/forms">
                     Forms Page
                 </Link>
-            </div>
+            </Wrapper>
         );
     }
 }
